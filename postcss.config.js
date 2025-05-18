fa83cb0156a9da9f2ec8e54c7a2c3050415c7e02
yudaDwiPrayoga <yupra2642@gmail.com>
2025-05-18 20:48:39 +0700

    perbaikan postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,30 @@
+// const config = {
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//   },
+// };
+// export default config;
+
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+
+
+
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+
+/** @type {import('postcss').Config} */
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+};
