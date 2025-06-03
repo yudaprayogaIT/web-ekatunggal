0a3447857c643a2dde0a1f85c49e1deb05c5b5f2
yudaDwiPrayoga <yupra2642@gmail.com>
2025-06-03 19:53:35 +0700

    Tata letak UI done, tinggal responsive dan perbaikan agar bisa muncul di vercel

--- src/components/karir/JobDetail.tsx
+++ src/components/karir/JobDetail.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import Image from "next/image";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import type { Job } from "@/data/jobs";
 import Link from "next/link";
 
--- src/components/produk/ProductCard.tsx
+++ src/components/produk/ProductCard.tsx
@@ -42,48 +42,46 @@ export default function ProductCard({ prod }: ProductCardProps) {
   };
 
   return (
-    <>
-      <HeaderComponent />
-      <div className="flex flex-row overflow-hidden hover:shadow-2xl rounded-3xl transition-shadow duration-300">
-        {/* <div className="flex flex-row border border-gray-100 rounded-3xl overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300"> */}
-        <div className="flex flex-2 flex-col">
-          {/* 1) Main Image */}
-          <div className="relative w-full h-80">
-            <Image
-              src={finalUrls[activeIndex]}
-              alt={prod.nama}
-              fill
-              style={{ objectFit: "fill" }}
-              className="rounded-3xl"
-            />
-          </div>
+    <div className="flex flex-row overflow-hidden hover:shadow-2xl rounded-3xl transition-shadow duration-300">
+      {/* <div className="flex flex-row border border-gray-100 rounded-3xl overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300"> */}
+      <div className="flex flex-2 flex-col">
+        {/* 1) Main Image */}
+        <div className="relative w-full h-80">
+          <Image
+            src={finalUrls[activeIndex]}
+            alt={prod.nama}
+            fill
+            style={{ objectFit: "fill" }}
+            className="rounded-3xl"
+          />
+        </div>
 
-          {/* 2) Thumbnail + tombol scroll */}
-          <div className="relative mx-15 mt-4 px-10">
-            {finalUrls.length > 3 && (
-              <button
-                onClick={() => scrollThumbnails("left")}
-                className="absolute left-0 top-1/2 z-10 transform -translate-y-1/2 bg-[#d9d9d98f] h-10 w-10 rounded-full hover:bg-[#d9d9d9] transition"
-                aria-label="Scroll kiri"
-              >
-                <Image
-                  src="/img/produk/arrow-left.png"
-                  alt="←"
-                  width={40}
-                  height={40}
-                />
-              </button>
-            )}
-
-            <div
-              ref={thumbContainerRef}
-              className="flex justify-between gap-x-4 max-w-140 space-x-8 overflow-x-hidden mb-2 snap-x snap-mandatory scrollbar-none px-4 pb-2"
+        {/* 2) Thumbnail + tombol scroll */}
+        <div className="relative mx-15 mt-4 px-10">
+          {finalUrls.length > 3 && (
+            <button
+              onClick={() => scrollThumbnails("left")}
+              className="absolute left-0 top-1/2 z-10 transform -translate-y-1/2 bg-[#d9d9d98f] h-10 w-10 rounded-full hover:bg-[#d9d9d9] transition"
+              aria-label="Scroll kiri"
             >
-              {finalUrls.map((url, idx) => (
-                <button
-                  key={idx}
-                  onClick={() => setActiveIndex(idx)}
-                  className={`
+              <Image
+                src="/img/produk/arrow-left.png"
+                alt="←"
+                width={40}
+                height={40}
+              />
+            </button>
+          )}
+
+          <div
+            ref={thumbContainerRef}
+            className="flex justify-between gap-x-4 max-w-140 space-x-8 overflow-x-hidden mb-2 snap-x snap-mandatory scrollbar-none px-4 pb-2"
+          >
+            {finalUrls.map((url, idx) => (
+              <button
+                key={idx}
+                onClick={() => setActiveIndex(idx)}
+                className={`
                 snap-center flex-shrink-0 w-35 h-20 rounded-md overflow-hidden border
                 ${
                   idx === activeIndex
@@ -91,51 +89,49 @@ export default function ProductCard({ prod }: ProductCardProps) {
                     : "border-gray-200"
                 } hover:brightness-110 transition
               `}
-                  aria-label={`Tampilkan gambar ${idx + 1}`}
-                >
-                  <Image
-                    src={url}
-                    alt={`${prod.nama} preview ${idx + 1}`}
-                    width={100}
-                    height={100}
-                    className="object-fill"
-                  />
-                </button>
-              ))}
-            </div>
-
-            {finalUrls.length > 3 && (
-              <button
-                onClick={() => scrollThumbnails("right")}
-                className="absolute right-0 top-1/2 z-10 transform -translate-y-1/2 bg-[#d9d9d98f] h-10 w-10 rounded-full hover:bg-[#d9d9d9] transition"
-                aria-label="Scroll kanan"
+                aria-label={`Tampilkan gambar ${idx + 1}`}
               >
                 <Image
-                  src="/img/produk/arrow-right.png"
-                  alt="→"
-                  width={40}
-                  height={40}
+                  src={url}
+                  alt={`${prod.nama} preview ${idx + 1}`}
+                  width={100}
+                  height={100}
+                  className="object-fill"
                 />
               </button>
-            )}
+            ))}
           </div>
-        </div>
-        {/* 3) Nama & Deskripsi */}
-        <div className="p-4 flex flex-1 flex-col justify-between font-[montserrat]">
-          <div className="relative w-full h-40 my-auto px-4">
-            <h3 className="text-xl font-bold capitalize text-gray-800">
-              {prod.nama}
-            </h3>
-            <h5 className="text-md font-medium text-gray-800">Detail Produk</h5>
-            {/* Garis horizontal abu */}
-            <div className="absolute left-4 top-13 h-[0.2] w-84 bg-gray-300" />
-            <p className="mt-1 text-gray-600 text-sm line-clamp-4">
-              {prod.deskripsi}
-            </p>
-          </div>
+
+          {finalUrls.length > 3 && (
+            <button
+              onClick={() => scrollThumbnails("right")}
+              className="absolute right-0 top-1/2 z-10 transform -translate-y-1/2 bg-[#d9d9d98f] h-10 w-10 rounded-full hover:bg-[#d9d9d9] transition"
+              aria-label="Scroll kanan"
+            >
+              <Image
+                src="/img/produk/arrow-right.png"
+                alt="→"
+                width={40}
+                height={40}
+              />
+            </button>
+          )}
         </div>
       </div>
-      <FooterComponent />
-    </>
+      {/* 3) Nama & Deskripsi */}
+      <div className="p-4 flex flex-1 flex-col justify-between font-[montserrat]">
+        <div className="relative w-full h-40 my-auto px-4">
+          <h3 className="text-xl font-bold capitalize text-gray-800">
+            {prod.nama}
+          </h3>
+          <h5 className="text-md font-medium text-gray-800">Detail Produk</h5>
+          {/* Garis horizontal abu */}
+          <div className="absolute left-4 top-13 h-[0.2] w-84 bg-gray-300" />
+          <p className="mt-1 text-gray-600 text-sm line-clamp-4">
+            {prod.deskripsi}
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
