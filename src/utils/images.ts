63f501181ff25b5616b34376fface4dce6ed85ee
yudaDwiPrayoga <yupra2642@gmail.com>
2025-06-04 09:52:25 +0700

    perbaikan vercel

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -11,17 +11,20 @@ export function buildImageUrl(rawPath: string): string {
 
   // 3) Jika rawPath sudah diawali "/public/files" atau "/files", cukup tambahkan BASE_URL
   if (rawPath.startsWith("/public/files") || rawPath.startsWith("/files")) {
-    return `${process.env.NEXT_PUBLIC_API_BASE_URL}${rawPath}`;
+    // return `${process.env.NEXT_PUBLIC_API_BASE_URL}${rawPath}`;
+    return `${"https://api-ekatalog.ekatunggal.com"}${rawPath}`;
   }
 
   // 4) Jika rawPath diawali '/', tapi bukan '/public/files' atau '/files',
   //    kita asumsikan ingin load path lain (misal "/assets/img.png")
   if (rawPath.startsWith("/")) {
-    return `${process.env.NEXT_PUBLIC_API_BASE_URL}${rawPath}`;
+    // return `${process.env.NEXT_PUBLIC_API_BASE_URL}${rawPath}`;
+    return `${"https://api-ekatalog.ekatunggal.com"}${rawPath}`;
   }
 
   // 5) Jika rawPath bukan diawali '/', kita anggap ini hanya nama file (misal "namafile.jpg").
   //    Karena lampiran Frappe tersedia di "/public/files/", kita prefix "/public/files/".
   const assumedPath = `/public/files/${rawPath}`;
-  return `${process.env.NEXT_PUBLIC_API_BASE_URL}${assumedPath}`;
+  // return `${process.env.NEXT_PUBLIC_API_BASE_URL}${assumedPath}`;
+  return `${"https://api-ekatalog.ekatunggal.com"}${assumedPath}`;
 }
