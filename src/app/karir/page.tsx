7d25a1a3679ede128d473294cf9995ed9249fe20
yudaDwiPrayoga <yupra2642@gmail.com>
2025-06-04 09:32:19 +0700

    perbaikan vercel

--- src/app/karir/page.tsx
+++ src/app/karir/page.tsx
@@ -1,22 +1,3 @@
-// // src/app/karir/page.tsx
-// import { FooterComponent } from "@/components/FooterComponent";
-// import HeaderComponent from "@/components/HeaderComponent";
-// import HeroKarirComponent from "@/components/karir/HeroKarirComponent";
-// // import { section } from "framer-motion/client";
-// import React from "react";
-
-// const page = () => {
-//   return (
-//     <>
-//       <HeaderComponent />
-//       <HeroKarirComponent />
-//       <FooterComponent />
-//     </>
-//   );
-// };
-
-// export default page;
-
 import { FooterComponent } from "@/components/FooterComponent";
 import HeaderComponent from "@/components/HeaderComponent";
 import HeroKarirComponent from "@/components/karir/HeroKarirComponent";
